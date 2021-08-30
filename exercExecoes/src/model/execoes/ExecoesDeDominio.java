95299b439afee71de68701f3ab35335ccde8efd8
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2021-08-30 19:43:29 -0300

    Solucao boa

---
+++ exercExecoes/src/model/execoes/ExecoesDeDominio.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package model.execoes;
+
+/**
+ * @author leo_dias
+ *
+ */
+public class ExecoesDeDominio extends Exception{
+	private static final long serialVersionUID = 1L;
+	
+	public ExecoesDeDominio(String msg) {
+		super(msg);
+	}
+	
+}
